293a2b2467626e56e4ee7e1c28d5f1962241c023
Shiyu <spadillezhang@gmail.com>
2015-11-04 19:21:01 -0500

    new change

---
+++ Assets/Draggable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public void OnbeingDrag(PointerEventData eventData){
+		Debug.Log("OnBeingDrag");
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Debug.Log ("OnDrag");
+
+		this.transform.position = eventData.position;
+	}
+
+	public void OnendDrag(PointerEventData evenData)
+	{
+		Debug.Log("OnEndDrag");
+	}
+
+}
