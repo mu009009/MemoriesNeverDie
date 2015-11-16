27d2cbc828c5460196e977e284cde699e3672269
suhas murthy <suhas murthy>
2015-11-16 06:07:17 -0500

    Draggable.cs/best_camera.cs

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -4,20 +4,8 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-	public void OnbeingDrag(PointerEventData eventData){
-		Debug.Log("OnBeingDrag");
-	}
+	
 
-	public void OnDrag(PointerEventData eventData)
-	{
-		Debug.Log ("OnDrag");
-
-		this.transform.position = eventData.position;
-	}
-
-	public void OnendDrag(PointerEventData evenData)
-	{
-		Debug.Log("OnEndDrag");
-	}
+	
 
 }
--- Assets/best_camera.cs
+++ Assets/best_camera.cs
@@ -2,14 +2,6 @@
 using System.Collections;
 
 public class best_camera : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
-	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
